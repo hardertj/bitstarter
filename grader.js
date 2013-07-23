9f277983fd2237c6a5a2aef6c115906aff365d72
Ubuntu <ubuntu@ip-172-31-47-120.us-west-2.compute.internal>
2013-07-23 04:04:31 +0000

    updated index.html

--- grader.js
+++ grader.js
@@ -4,8 +4,10 @@ var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var PROCESSFILE_DEFAULT = 'process.html';
 var myArgs = process.argv.slice(2);
 
 var assertFileExists = function(infile) {
@@ -40,21 +42,44 @@ var clone = function(fn) {
 	return fn.bind({});
 };
 
+var buildfn = function(){
+	var response2console = function(result, response) {
+		//console.error(" in response2console\n");
+		//console.error(program.file); 
+		fs.writeFileSync(program.file, result); 
+		processfile(program.file); 
+	};
+	return response2console;
+};
+
+var processfile = function(filename) {
+	//console.error("in function processfile\n");
+	var checkJson = checkHtmlFile(filename, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+}
+
 if( require.main == module) {
 	program
 		.option('-c, --checks checks.json', './Desktop/', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file index.html', './Desktop/', clone(assertFileExists), HTMLFILE_DEFAULT)
-		//.option('-u, --url http://localhost:8888')
+		.option('-u, --url http://localhost:8890', 'http://localhost:8890')
 		.parse(process.argv);
-        console.log(myArgs[2]);
-	if( myArgs[2] == '--url')
-		rest.get(myArgs[3].on('complete'), response2console);
-		console.log("done");
+        
+	var filename = ""; 
+	
+	if( myArgs[2] == '--url') {
+		//console.error(" processing url\n");
+		program.file = PROCESSFILE_DEFAULT; 
+		var response2console = buildfn();
+		rest.get(myArgs[3]).on('complete', response2console);
+	}		
 	else
 	{
-		var checkJson = checkHtmlFile(program.file, program.checks);
-		var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+		//console.error("processing file\n"); 
+		//console.error(myArgs[3]); 
+		program.file = myArgs[3]		
+		processfile(program.file); 
 	}
 }
 else
